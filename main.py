c6881b1c169c5a385d49602b6045aec2bd1d9bfb
Li Chun Hua <jim870718@gmail.com>
2021-11-06 13:24:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,49 @@
+""" $LAN=Python """
+
 from tkinter import *
 from tkinter import ttk
 
-
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
-    except ValueError:
-        pass
+import file_progress as fp
 
 
 root = Tk()
-root.title("Feet to Meters")
+root.title("Voronoi")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+mainframe = ttk.Frame(root, padding="10 10 10 10")
+mainframe.grid(column=0, row=0)
 
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+# 畫圖區域
+main_canvas = Canvas(mainframe, width=600, height=600, background="black")
+main_canvas.grid(column=0, row=0)
 
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+# 放按鈕的容器
+sideframe = ttk.Frame(root, padding="10 10 10 10")
+sideframe.grid(column=1, row=0)
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(
-    column=3, row=3, sticky=W
+file_path = StringVar()
+file_name_lb = ttk.Label(sideframe, textvariable=file_path)
+read_file_btn = ttk.Button(
+    sideframe, width=16, text="open file", command=lambda: fp.open_file(file_path)
 )
+output_file_btn = ttk.Button(
+    sideframe, width=16, text="save result to file", command=None
+)
+# TODO : command
+show_graph_btn = ttk.Button(sideframe, width=16, text="show graph", command=None)
+show_result_btn = ttk.Button(sideframe, width=16, text="show result", command=None)
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+next_step_btn = ttk.Button(sideframe, width=16, text="run next step", command=None)
+clear_canvas_btn = ttk.Button(sideframe, width=16, text="clear canvas", command=None)
 
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
 
-feet_entry.focus()
-root.bind("<Return>", calculate)
+# sideframe layout
+file_name_lb.grid(row=0)
+read_file_btn.grid(row=1)
+output_file_btn.grid(row=2)
+show_graph_btn.grid(row=3)
+show_graph_btn.grid(row=4)
+show_result_btn.grid(row=5)
+next_step_btn.grid(row=6)
+clear_canvas_btn.grid(row=7)
 
 root.mainloop()
