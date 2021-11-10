f9e765fa36f105af787923058702d99738a9e550
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-11-10 14:07:55 +0800

    structure class

--- voronoi_algo_test.py
+++ voronoi_algo_test.py
@@ -2,15 +2,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %%
-p1 = (300, 300)
-p2 = (400, 400)
+from class_def import *
 
-#%%
+# %%
+p1 = P(100, 300)
+p2 = P(200, 400)
+p3 = (p1 + p2) / 2
+print(p3)
+# %%
 fig = plt.figure(figsize=(10, 10))
 ax = plt.axes()
 plt.xlim(0, 600)
 plt.ylim(0, 600)
-plt.scatter([300, 400], [300, 400])
-plt.plot([300, 400], [300, 400])
+
+p1.draw()
+p2.draw()
+
 plt.show()
+# %%
