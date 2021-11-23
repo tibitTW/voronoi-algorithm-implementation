32408a8aaf7ba59c59868b4e716b18376e1e6763
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-11-23 13:45:05 +0800

    .

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class sc:
         self.sideframe = ttk.Frame(self.root, padding="0 10 10 10")
         self.sideframe.grid(column=1, row=0)
         self._init_sideframe_elements()
-        self._init_sideframe_layout()
+        self.init_sideframe_layout()
 
         # record points
         self.points = []
@@ -35,21 +35,21 @@ class sc:
         self.file_path = StringVar()
         self.file_name_lb = ttk.Label(self.sideframe, textvariable=self.file_path)
         self.read_file_btn = ttk.Button(self.sideframe, width=16, text="read file", command=lambda: fp.open_in_file(self.file_path))
-        self.show_graph_btn = ttk.Button(self.sideframe, width=16, text="next set", command=None)  # TODO : commands
-        self.next_step_btn = ttk.Button(self.sideframe, width=16, text="step by step", command=None)  # TODO : commands
-        self.show_result_btn = ttk.Button(self.sideframe, width=16, text="run", command=None)  # TODO : commands
-        self.output_graph_file_btn = ttk.Button(self.sideframe, width=16, text="save image", command=None)  # TODO : commands
-        self.read_graph_file_btn = ttk.Button(self.sideframe, width=16, text="read image", command=None)  # TODO : commands
+        self.next_set_btn = ttk.Button(self.sideframe, width=16, text="next set", command=None)  # TODO : commands
+        self.step_by_step_btn = ttk.Button(self.sideframe, width=16, text="step by step", command=None)  # TODO : commands
+        self.run_btn = ttk.Button(self.sideframe, width=16, text="run", command=None)  # TODO : commands
+        self.write_graph_file_btn = ttk.Button(self.sideframe, width=16, text="save image", command=lambda: fp.save_vd_file())
+        self.read_graph_file_btn = ttk.Button(self.sideframe, width=16, text="read image", command=lambda: fp.open_vd_file())
         self.clear_canvas_btn = ttk.Button(self.sideframe, width=16, text="clear canvas", command=self.clean_canvas)
 
     # initialize sideframe layout
-    def _init_sideframe_layout(self):
+    def init_sideframe_layout(self):
         self.file_name_lb.grid(row=0)
         self.read_file_btn.grid(row=1)
-        self.show_graph_btn.grid(row=2)
-        self.next_step_btn.grid(row=3)
-        self.show_result_btn.grid(row=4)
-        self.output_graph_file_btn.grid(row=5)
+        self.next_set_btn.grid(row=2)
+        self.step_by_step_btn.grid(row=3)
+        self.run_btn.grid(row=4)
+        self.write_graph_file_btn.grid(row=5)
         self.read_graph_file_btn.grid(row=6)
         self.clear_canvas_btn.grid(row=7)
 
@@ -70,6 +70,7 @@ class sc:
 
 if __name__ == "__main__":
     main_sc = sc()
+    main_sc.print_point(-10, -10)
     main_sc.mainloop()
 
 
