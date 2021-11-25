5eb1edc32a3381dd023d8bbc24b7988d7854a1e1
Li Chun Hua <39118448+tibitTW@users.noreply.github.com>
2021-11-25 15:57:31 +0800

    增加座標運算相關function

--- tools.py
+++ tools.py
@@ -124,17 +124,69 @@ class F:
 
 
 # 找中垂線
-# def get_bisection(p1: P, p2: P):
-#     p3 = (p1 + p2) / 2
-#     x_diff = p1.getX() - p2.getX()
-#     y_diff = p1.getY() - p2.getY()
-#     a = -x_diff / y_diff
-#     b = p3.getY() - a * p3.getX()
-#     return F(a, b)
 def get_bisection(p1, p2):
+    if p1[0] == p2[0]:  # 水平線
+        y = (p1[1] + p2[1]) / 2
+        return 0, y, 600, y
+    if p1[1] == p2[1]:  # 垂直線
+        x = (p1[0] + p2[0]) / 2
+        return x, 0, x, 600
+
     p3 = ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
-    x_diff = p1[0] - p2[0]
-    y_diff = p1[1] - p2[1]
-    a = -x_diff / y_diff
-    b = p3[1] - a * p3[0]
-    return F(a, b)
+    v = (p1[1] - p2[1], p2[0] - p1[0])
+
+    pA = (0, p3[1] - p3[0] * v[1] / v[0])
+    pB = (p3[0] - p3[1] * v[0] / v[1], 0)
+    pC = (600, p3[1] + (600 - p3[0]) * v[1] / v[0])
+    pD = (p3[0] + (600 - p3[1]) * v[0] / v[1], 600)
+
+    if v[0] * v[1] < 0:  # BC比, AD比
+        if pB[0] < 0 or pB[0] > 600:
+            pS = pC
+        else:
+            pS = pB
+        if pA[0] < 0 or pA[0] > 600:
+            pE = pD
+        else:
+            pE = pA
+    else:  # AB比，CD比
+        if pA[0] < 0 or pA[0] > 600:
+            pS = pB
+        else:
+            pS = pA
+        if pC[0] < 0 or pC[0] > 600:
+            pE = pD
+        else:
+            pE = pC
+
+    return pS[0], pS[1], pE[0], pE[1]
+
+
+def cross(o, a, b):
+    return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+
+def intersect1D(a1: int, a2: int, b1: int, b2: int):
+    a1, a2 = min(a1, a2), max(a1, a2)
+    b1, b2 = min(b1, b2), max(b1, b2)
+
+    return max(a1, b1) <= min(a2, b2)
+
+
+def intersect(a1: list, a2: list, b1: list, b2: list):
+    return (
+        intersect1D(a1[0], a2[0], b1[0], b2[0])
+        and intersect1D(a1[1], a2[1], b1[1], b2[1])
+        and cross(a1, a2, b1) * cross(a1, a2, b2) <= 0
+        and cross(b1, b2, a1) * cross(b1, b2, a2) <= 0
+    )
+
+
+def intersection(a1: list, a2: list, b1: list, b2: list):
+    a = (a2[0] - a1[0], a2[1] - a1[1])
+    b = (b2[0] - b1[0], b2[1] - b1[1])
+    s = (b1[0] - a1[0], b1[1] - a1[1])
+
+    cross_sb = cross((0, 0), s, b)
+    cross_ab = cross((0, 0), a, b)
+    return (a1[0] + a[0] * cross_sb / cross_ab, a1[1] + a[1] * cross_sb / cross_ab)
